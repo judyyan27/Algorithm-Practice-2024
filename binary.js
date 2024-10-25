1d73dcc1c0f884ae930ad1b05429da6654efdf3b
Judy <158211720+judyyan27@users.noreply.github.com>
2024-10-25 12:26:32 -0400

    Update binary.js

--- binary.js
+++ binary.js
@@ -5,17 +5,33 @@
 //Using Pseudocode write a function to convert binary to decimal
 
 function toDecimal(binary) {
-  const num = [128, 64, 32, 16, 8, 4, 2, 1];
+  const num = [1, 2, 4, 8, 16, 32, 64, 128];
   let dec = 0;
-
+  b = binary.reverse;
+  console.log(b);
   for (let i = 0; i <= 8; i++) {
-    if (binary[i] === "1") {
+    if (binary[binary.length - i] === "1") {
       dec = dec + num[i];
     }
   }
   console.log(dec);
 }
 
-console.log(toDecimal("11111111"));
+console.log(toDecimal("1011"));
 
 //00000000
+/* 
+function binaryToDec(binary) {
+  let decimal = 0;
+  //     -- reverses the string
+  for (let i = 0; i < binary.length; i++) {
+    if (binary[binary.length - 1] === "1") {
+      decimal = decimal + Math.pow(2, i);
+    }
+  }
+  return decimal;
+}
+console.log(binaryToDec("1101"));
+
+//assume decimal is a string
+ */
